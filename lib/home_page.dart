b4a649dd6581fc6e3c9fb43972eaaed1b8324257
3aMoOoRy <omarelbadry19200@gmail.com>
2021-10-28 02:22:01 +0200

    finish home screen add a widget for each tab

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,68 @@
+import 'package:aslami_app/home/Hadeth/hadeth_tab.dart';
+import 'package:aslami_app/home/Quran/quran_tab.dart';
+import 'package:aslami_app/home/Radio/radio_tab.dart';
+import 'package:aslami_app/home/Tasbeh/tasbeh_tab.dart';
+import 'package:aslami_app/main.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-static const String routeName="home";
+class HomePage extends StatefulWidget {
+  static const String routeName = "home";
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+int CurrentIndex=0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    body: Container(),
+    return Stack(
+      children:  [
+        Image.asset('assets/images/main_background.png'),
+         Scaffold(
+           appBar: AppBar(
+             centerTitle: true,
+             title: Text('Islami'),
+           ),
+           bottomNavigationBar: Theme(
+             data: Theme.of(context).copyWith(
+               canvasColor: MyThemeData.PrimaryColor
+             ),
+             child: BottomNavigationBar(
+               currentIndex: CurrentIndex,
+               onTap: (index) {
+                 CurrentIndex=index;
+                 setState(() {
+                 });
+               },
+               items: const [
+                 BottomNavigationBarItem(
+                     icon:ImageIcon(AssetImage('assets/images/icon_radio.png')) ,
+                 label: 'Radio'),
+                 BottomNavigationBarItem(
+                     icon:ImageIcon(AssetImage('assets/images/icon_sebha.png')) ,
+                 label: 'Tasbeh'),
+                 BottomNavigationBarItem(
+                     icon:ImageIcon(AssetImage('assets/images/icon_hadeth.png')) ,
+                 label: 'Hadeth'),
+                 BottomNavigationBarItem(
+                     icon:ImageIcon(AssetImage('assets/images/icon_quran.png')) ,
+                 label: 'Quran'),
+               ],
+             ),
+           ),
+           body: Container(
+             child: views[CurrentIndex],
+           ),
+        ),
+      ],
     );
   }
+  List<Widget>views=[
+    RadioTab(),
+    TasbehTab(),
+    HadethTab(),
+    QuranTab(),
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,45 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+class MyThemeData{
+  static const Color PrimaryColor=Color.fromARGB(255, 183, 147, 95);
+  static const Color PrimaryColorDark=Color.fromARGB(255, 15, 20, 36);
+  static final  ThemeData LightTheme= ThemeData(
+    scaffoldBackgroundColor: Colors.transparent,
+    primaryColor:MyThemeData.PrimaryColor ,
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+        )
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+     selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.white,
+    ),
+  );
+  static final  ThemeData DarkTheme= ThemeData(
+    scaffoldBackgroundColor: Colors.transparent,
+    primaryColor:MyThemeData.PrimaryColor ,
+    appBarTheme: AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+        )
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.white,
+    ),
+  );
+
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,6 +51,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: ('Islami'),
+      theme: MyThemeData.LightTheme,
+      darkTheme: MyThemeData.DarkTheme,
       routes: {
         HomePage.routeName:(buildContext)=>HomePage()},
       initialRoute: HomePage.routeName,
